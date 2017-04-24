179fa7f11764c51949f1d9aa4148502e55e814dc
zerjioang <sergio.anguita@opendeusto.es>
2017-04-24 18:32:38 +0200

    MPI example working with numbers and sum function

--- cracker/mpi/crackermpi.c
+++ cracker/mpi/crackermpi.c
@@ -30,7 +30,14 @@ static const char alpha_space[] = "abcdefghijklmnopqrstuvwxyz ";
 
 //global variables for MPI
 int numtasks, rank, len; 
+int tasks_to_do;
 MPI_Status Stat;
+
+//for demo
+#define DEMO_I 5
+int i, recibido[DEMO_I], total;
+int a[DEMO_I];
+int sumatorio = 0;
 
 void init_mpi(int argc, char **argv){
   MPI_Init(&argc,&argv);
@@ -99,140 +106,65 @@ int estimateLength(int value, int min, int max, int charset_length){
 }
 #endif
 
-int execute(int start_value, int min, int max, char* target, const char* charset)
-{
+int execute(int start_value, int min, int max, char* target, const char* charset){
   //generated from globals
   int charset_length = strlen(charset);
 
   //calculate key space
   long key_space = 0L;
 
-  if(rank == MASTER){
-	//this code is only executed by master
-	  #ifdef MULTIPLE_LEN
-	    int i=0;
-	    for(i = min; i <= max; i++){
-	      long value = ipow(charset_length, i);
-	      key_space += value;
-	      #ifdef DEBUG
-		showme();
-		printf("\t\tSingle key space for length %d is:\t%ld\n", i, value);
-	      #endif
-	    }
-	  #else
-	    key_space = ipow(charset_length, max);
-	  #endif
-	  printf("\t\tBruteforce key space:\t%ld\n", key_space);
-  }
-  //share workload among different threads
-  //example: MPI_Scatter(a, 25, MPI_INT, recibido, 25, MPI_INT, 0, MPI_COMM_WORLD);
-
-	//###############
-	//this is the parallel area
-	//###############
-
-	showme();
-
-
-  //current password length
-  int password_length;
-
-  //key index
-  int iterate;
-
-  //generate keyspace
-  int last_passw_len = min;
-  int payload = 0;
-
-  //temp variable to hold result
-  char* found;
-
-  //password cracked detection condition. per thread
-  int password_cracked = NOT_CRACKED;
-  long attempts = 0;
-  //stop condition. multithread shared
-  int stop_condition = 0;
-
-  //start cracking until found
-  for(iterate = start_value ; iterate < key_space; iterate++){
-    if(stop_condition == KEEP_RUNNING) {
+  //this code is only executed by master
+  #ifdef MULTIPLE_LEN
+    int i=0;
+    for(i = min; i <= max; i++){
+      long value = ipow(charset_length, i);
+      key_space += value;
       #ifdef DEBUG
-         printf("[Thread %d of %d] - %d %d \n", -1, -1, iterate, stop_condition);
-        #endif
-        int idx = iterate;
-        #ifdef MULTIPLE_LEN
-          //password length will be variable depending on given key and min max values.
-          int password_length = estimateLength(idx, min, max, charset_length);
-          if(password_length != last_passw_len){
-            payload = ipow(charset_length, last_passw_len);
-            idx = iterate - payload;
-          }
-        #endif
-        //get key given an index
-        char *key = getKeyIndex(idx, password_length, charset);
-        char* hash = compute(key, password_length);
-        #ifdef DEBUG
-         printf("[Thread %d of %d] - Iteration: %d Key: %s MD5: %s\n", -1, -1, iterate, key, hash);
-        #endif
-        //compare it
-        if(strcmp(hash, target)==0){
-          found = key;
-          #ifdef DEBUG
-            printf("[Thread %d of %d] - FOUND THE PASSWD: %d Key: %s MD5: %s\n", -1, -1, iterate, key, hash);
-          #endif
-          //release hash
-          free(hash);
-          hash = NULL;
-          //pass cracked. stop
-          password_cracked = PAWNED;
-          stop_condition = STOP;
-        }
-
-        //TODO decide how many times a message must be send
-        //send MPI messages and receive them
-        //send found status
-	int i; 
-        for(i=0; i < numtasks; i++){
-          if ( i != rank )
-            MPI_Send(&found, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-        } 
-
-        //received found status
-        for ( i = 0; i < numtasks; i++){
-          if ( found == 0 && i != rank )
-            MPI_Recv(&found, 1, MPI_INT,i,1,MPI_COMM_WORLD,&Stat);  
-        }
-
-      //increase attempt count
-      attempts = attempts + 1;
-
-      //check if current thread found passwd or any other thread found it
-      if(password_cracked == PAWNED || stop_condition == STOP){
-        stop_condition = STOP;
+      showme();
+      if(rank == MASTER){
+        printf("\t\tSingle key space for length %d is:\t%ld\n", i, value);
       }
+      #endif
     }
-  }//end for
-
-  //example reduce: MPI_Reduce(&sumatorio, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  #else
+    key_space = ipow(charset_length, max);
+  #endif
   if(rank == MASTER){
-    //this code is executed by master thread only
-    //show result
-    if(password_cracked == PAWNED){
-      printf("\n\n\t#################\n");
-      printf("\tPassword cracked:\n");
-      printf("\t#################\n\n" );
-      printf("\n\tAttepts: %li \t of \t %li\n", attempts, key_space);
-      printf("\tPassword is: %s\n\n", found);
-      //release found key
-      free(found);
-      found = NULL;
-    }
-    else{
-      printf("\n\n\tNO HASH FOUND. SORRY :(\n\n");
-    }
-  }  
-  MPI_Finalize();
+    printf("\t\tBruteforce key space:\t%ld\n", key_space);
+  }
 
+  if(rank == MASTER){
+      printf("\t\tInicializando datos...\n");
+      for(i = 0; i < DEMO_I; i++){
+          a[i] = i + 1;
+      }
+      printf("\t\tArray de datos inicializado\n");
+  }
+
+  int pass_per_tasks = key_space / numtasks;
+  int restante = key_space % numtasks;
+  int reparto = DEMO_I/numtasks;
+  MPI_Scatter(a, reparto, MPI_INT, recibido, reparto, MPI_INT, 0, MPI_COMM_WORLD);
+
+  for(i = 0; i < reparto; i++){
+      sumatorio += recibido[i];
+  }
+
+  //printf("Soy el proceso %d de %d, (%d)\n", rank, numtasks, len);
+  //printf("Mi sumatorio es: %d\n", sumatorio);
+  MPI_Reduce(&sumatorio, &total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+  //procesar lo que se ha quedado sin repartir por master
+  if(rank == MASTER){
+    if(restante > 0){
+      //procesar las combinaciones que quedan  
+      for(i = DEMO_I - reparto; i < DEMO_I; i++){
+        total += a[i];
+      }
+      printf("El sumatorio total vale:\t%d\n", total);
+    }
+  }
+  MPI_Finalize();
   return SUCCESS_CODE;
 }
 
@@ -264,30 +196,36 @@ int brute_force(int start_value, char* min, char* max, char* hash, char* charset
 
 int main(int argc, char **argv)
 {
+
+  char* execution_path;
+  char* min_size;
+  char* max_size;
+  char* cores;
+  char* charset;
+  char* hash;
+  int start_value;
+
   init_mpi(argc, argv);
 
+  showme();
+  
   //sustituir esto por getops
-  char* execution_path = argv[0];
-  printf("\texecution_path:\t%s\n", execution_path);
-  char* min_size = argv[1];
-  printf("\tmin_size:\t%s\n", min_size);
-  char* max_size = argv[2];
-  printf("\tmax_size:\t%s\n", max_size);
-  char* cores = argv[3];
-  printf("\tthreads:\t%s\n", cores);
-  char* charset = argv[4];
-  printf("\tcharset:\t%s\n", charset);
-  char* hash = argv[5];
-  printf("\ttarget:\t%s\n", hash);
-  int start_value = 0;
+  execution_path = argv[0];
+  min_size = argv[1];
+  max_size = argv[2];
+  cores = argv[3];
+  charset = argv[4];
+  hash = argv[5];
+  start_value = 0;
 
-  //set mpi threads
-  int thread_num = atoi(cores);
-  if(thread_num >= 1){
-    printf("\tDedicated threads to cracking:\t%d\n", thread_num);
+  if( rank == MASTER ){
+    printf("\texecution_path:\t%s\n", execution_path);
+    printf("\tmin_size:\t%s\n", min_size);
+    printf("\tmax_size:\t%s\n", max_size);
+    printf("\tthreads:\t%s\n", cores);
+    printf("\tcharset:\t%s\n", charset);
+    printf("\ttarget:\t%s\n", hash);
   }
-  else{
-    thread_num = DEFAULT_THREAD_NUM;
-  }
+  
   return brute_force(start_value, min_size, max_size, hash, charset);
 }
